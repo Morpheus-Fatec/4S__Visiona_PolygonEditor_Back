8d3b10473b25f1a28b9308fc9259936965b0e7a4
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-03-17 18:27:58 -0300

    feat (#5): Criação entidade fazenda (Farm)

---
+++ src/main/java/com/morpheus/backend/entity/Farm.java
@@ -0,0 +1,37 @@
+package com.morpheus.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "farm")
+public class Farm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_farm")
+    private Long id_farm;
+
+    @Column(name = "name_farm")
+    private String farmName;
+
+    @Column(name = "city_farm")
+    private String farmCity;
+
+    @Column(name = "state_farm")
+    private String farmState;
+
+}
