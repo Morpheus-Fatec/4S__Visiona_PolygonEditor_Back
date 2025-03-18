141cf86bbe3ae132bf812bac8df0f0dee74d0d13
MateusMSoares <mateusmsoares@protonmail.com>
2025-03-18 18:14:22 -0300

    [fix]: Correção ortográfica no swagger

--- src/main/java/com/morpheus/backend/config/SwaggerConfig.java
+++ src/main/java/com/morpheus/backend/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Morpheus-olygonEditor")
+                        .title("Morpheus-PolygonEditor")
                         .version("1.0"));
     }
 }
