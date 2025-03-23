af0331bedfeca400ecd82d31589f9b40f4753346
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2025-03-23 20:40:12 -0300

    Feat(#05): Iniciando entidade cultura

---
+++ src/main/java/com/morpheus/backend/controller/CultureController.java
@@ -0,0 +1,33 @@
+package com.morpheus.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.morpheus.backend.service.CultureService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/cultures")
+public class CultureController {
+    @Autowired
+    private CultureService cultureService;
+    
+    @PostMapping
+    public String createCulture(@RequestBody String cultureName) {
+        return cultureService.createCulture(cultureName);
+    }
+
+    @GetMapping("path")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+    
+}
