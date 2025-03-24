acbf16ee1b72b694a2fca9d54ebb0612b7c833aa
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-03-23 23:26:39 -0300

    Fix(#05): Refatorado método createCulture, criado métodos updateCultureName e deleteCulture

--- src/main/java/com/morpheus/backend/service/CultureService.java
+++ src/main/java/com/morpheus/backend/service/CultureService.java
@@ -5,26 +5,28 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.morpheus.backend.DTO.CultureDTO;
 import com.morpheus.backend.entity.Culture;
 import com.morpheus.backend.repository.CultureRepository;
 import com.morpheus.exceptions.DefaultException;
 
 @Service
 public class CultureService {
+
     @Autowired
     CultureRepository cultureRepository;
-    
-    public String createCulture(String cultureName){
-        try {   
-            if(cultureName.isEmpty()){
+
+    public String createCulture(CultureDTO cultureName){
+        try {
+            if(cultureName.getName().isEmpty()){
                 throw new Exception();
             }
-
+            
             Culture culture = new Culture();
-            culture.setName(cultureName);
+            culture.setName(cultureName.getName());
             cultureRepository.save(culture);
 
-            return "Cultura criada com sucesso.";
+            return "Cultura de " + cultureName.getName() + " criada com sucesso.";
         } catch (Exception e) {
             throw new DefaultException("Não foi possível criar a cultura.");
         }
@@ -33,7 +35,7 @@ public class CultureService {
     public List<Culture> getAllCultures(){
         try {
             List<Culture> cultures = cultureRepository.findAll();
-
+            
             if(cultures.isEmpty()){
                 throw new Exception();
             }
@@ -55,6 +57,42 @@ public class CultureService {
             return culture;
         } catch (Exception e) {
             throw new DefaultException("Não foi possível encontrar a cultura.");
+        }
+    }
+
+    public String updateCultureName(Long id, String cultureName){
+        try {
+            String oldName = "";
+            Culture culture = cultureRepository.getCultureById(id);
+
+            if(culture == null){
+                throw new Exception();
+            }
+            if(cultureName.isEmpty()){
+                throw new Exception();
+            }
+
+            oldName = culture.getName();
+            culture.setName(cultureName);
+            cultureRepository.save(culture);
+
+            return "Nome da cultura atualizado de " + oldName + " para " + cultureName + ".";
+        }
+        catch (Exception e) {
+            throw new DefaultException("Não foi possível atualizar o nome da cultura.");
+        }
+    }
+
+    public String deleteCulture(Long id){
+        try {
+            Culture culture = cultureRepository.getCultureById(id);
+            if(culture == null){
+                throw new Exception();
+            }
+            cultureRepository.delete(culture);
+            return "Cultura de " + culture.getName() + " deletada com sucesso.";
+        } catch (Exception e) {
+            throw new DefaultException("Não foi possível deletar a cultura.");
         }
     }
 }
