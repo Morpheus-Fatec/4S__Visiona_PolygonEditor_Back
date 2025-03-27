5676b94e5a2fa052aa0b9782c025285cb01652cc
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-03-27 18:54:20 -0300

    Feature(#05): Criado DTO para Soil

---
+++ src/main/java/com/morpheus/backend/DTO/SoilDTO.java
@@ -0,0 +1,14 @@
+package com.morpheus.backend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SoilDTO {
+    private String name;
+}
