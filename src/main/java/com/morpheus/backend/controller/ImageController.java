a7f5108b36ece88e2f91ddb83f1b020b95ae7ea3
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-03-27 18:52:50 -0300

    Fix(#05): Removido método não utilizado.

--- src/main/java/com/morpheus/backend/controller/ImageController.java
+++ src/main/java/com/morpheus/backend/controller/ImageController.java
@@ -3,15 +3,12 @@ package com.morpheus.backend.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.morpheus.backend.DTO.ImageDTO;
 import com.morpheus.backend.entity.Image;
 import com.morpheus.backend.service.ImageService;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -20,10 +17,6 @@ public class ImageController {
     @Autowired
     private ImageService imageService;
 
-    @PostMapping
-    public void createImage(@RequestBody ImageDTO imageUrl) throws Exception {
-        imageService.createImage(imageUrl);
-    }
 
     @GetMapping
     public List<Image> getAllImagesUrl() throws Exception {
