778511ee99b247e7138c211428e290ddc6695eaf
Isaque <isaquesc2@gmail.com>
2025-03-29 17:40:30 -0300

    feat: Implementado Filtro de Busca de Talhões

--- src/main/java/com/morpheus/backend/controller/FieldController.java
+++ src/main/java/com/morpheus/backend/controller/FieldController.java
@@ -1,6 +1,7 @@
 package com.morpheus.backend.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.morpheus.backend.DTO.GeoJsonView.FeatureCollectionDTO;
@@ -21,10 +22,18 @@ public class FieldController {
     private FieldService fieldService;
 
     @GetMapping("/featureCollectionSimple")
-    public ResponseEntity<FeatureCollectionSimpleDTO> getAllFeatureCollectionSimpleDTO() throws IllegalAccessError {
-        FeatureCollectionSimpleDTO featureCollectionSimpleDTO = fieldService.getAllFeatureCollectionSimpleDTO();
-
-        return ResponseEntity.ok(featureCollectionSimpleDTO);
+    public ResponseEntity<FeatureCollectionSimpleDTO> getAllFeatureCollectionSimpleDTO(
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) String soil,
+        @RequestParam(required = false) String status,
+        @RequestParam(required = false) String culture,
+        @RequestParam(required = false) String harvest,
+        @RequestParam(required = false) String farmName
+    ) {
+        FeatureCollectionSimpleDTO featureCollection = fieldService.getAllFeatureCollectionSimpleDTO(
+            name, soil, status, culture, harvest, farmName
+        );
+        return ResponseEntity.ok(featureCollection);
     }
 
     @GetMapping("/featureCollection/{id}")
