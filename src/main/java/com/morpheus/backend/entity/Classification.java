fecb6415417757d291fbe0598d14494c03eacfbf
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-04-10 19:13:04 -0300

    Refactor(#29): Alterado tipo dos atributos coordenadas para MultiPolygon com suporte a dados geométricos e removido espaços desnecessários.

--- src/main/java/com/morpheus/backend/entity/Classification.java
+++ src/main/java/com/morpheus/backend/entity/Classification.java
@@ -2,6 +2,8 @@ package com.morpheus.backend.entity;
 
 import java.math.BigDecimal;
 
+import org.locationtech.jts.geom.MultiPolygon;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,19 +21,19 @@ public class Classification {
     @Column(name = "id_classificacao")
     private Long id;
 
-    @Column(name = "coordenadas_atuais", columnDefinition = "TEXT")
-    private String currentCoordinates;
+    @Column(name = "coordenadas_atuais", columnDefinition = "geometry(MultiPolygon, 4326)")
+    private MultiPolygon currentCoordinates;
 
-    @Column(name = "coordenadas_originais", nullable = false, columnDefinition = "TEXT")
-    private String originalCoordinates;
+    @Column(name = "coordenadas_originais", nullable = false, columnDefinition = "geometry(MultiPolygon, 4326)")
+    private MultiPolygon originalCoordinates;
 
     @ManyToOne
     @JoinColumn(name = "id_talhao", nullable = false)
     private Field field;
 
     @ManyToOne
-    @JoinColumn(name = "id_classe", referencedColumnName = "id_classe", nullable = false) 
-    private ClassEntity classEntity;    
+    @JoinColumn(name = "id_classe", referencedColumnName = "id_classe", nullable = false)
+    private ClassEntity classEntity;
 
     @Column(name = "area", nullable = false, precision = 10, scale = 2)
     private BigDecimal area;
