a61a6a7770583e3f6b7c2165b844fc257a0c8efa
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-04-10 19:27:44 -0300

    Feat(#29): Implementado conversão de dados geométricos para formato GeoJSON no GeometryDTO, para exibição correta no front

--- src/main/java/com/morpheus/backend/DTO/GeoJsonView/GeometryDTO.java
+++ src/main/java/com/morpheus/backend/DTO/GeoJsonView/GeometryDTO.java
@@ -1,5 +1,9 @@
 package com.morpheus.backend.DTO.GeoJsonView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.Data;
 
 @Data
@@ -8,4 +12,15 @@ public class GeometryDTO {
     private String type = "MultiPolygon";
     private String coordinates;
 
+    public void convertToGeoJson(String geoJson) throws JsonProcessingException{
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode geoJsonNode = objectMapper.readTree(geoJson);
+
+        // Acessando a chave "coordinates"
+        JsonNode coordinatesNode = geoJsonNode.get("coordinates");
+
+        // Se quiser como String formatada
+        String coordinatesJson = coordinatesNode.toString();
+        this.coordinates = coordinatesJson;
+    }
 }
