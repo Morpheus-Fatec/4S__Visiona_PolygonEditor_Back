5e92c5860ee2b2f56467f0ff9bdda6e745eddff8
gabriel15asouza <gabalves.souza@gmail.com>
2025-04-15 01:59:32 -0300

    [feat}: adicao de paginacao a solicitacao

--- src/main/java/com/morpheus/backend/controller/FieldController.java
+++ src/main/java/com/morpheus/backend/controller/FieldController.java
@@ -4,8 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.morpheus.backend.DTO.PaginatedFieldResponse;
 import com.morpheus.backend.DTO.GeoJsonView.FeatureCollectionDTO;
 import com.morpheus.backend.DTO.GeoJsonView.FeatureCollectionSimpleDTO;
+import com.morpheus.backend.DTO.GeoJsonView.FeatureSimpleDTO;
 import com.morpheus.backend.service.FieldService;
 
 
@@ -21,18 +23,20 @@ public class FieldController {
     private FieldService fieldService;
 
     @GetMapping("/featureCollectionSimple")
-    public ResponseEntity<FeatureCollectionSimpleDTO> getAllFeatureCollectionSimpleDTO(
+    public ResponseEntity<PaginatedFieldResponse<FeatureSimpleDTO>> getAllFeatureCollectionSimpleDTO(
         @RequestParam(required = false) String name,
         @RequestParam(required = false) String soil,
         @RequestParam(required = false) String status,
         @RequestParam(required = false) String culture,
         @RequestParam(required = false) String harvest,
-        @RequestParam(required = false) String farmName
+        @RequestParam(required = false) String farmName,
+        @RequestParam (defaultValue = "1")int page, 
+        @RequestParam(defaultValue = "50") int itens
     ) {
-        FeatureCollectionSimpleDTO featureCollection = fieldService.getAllFeatureCollectionSimpleDTO(
-            name, soil, status, culture, harvest, farmName  
-        );
-        return ResponseEntity.ok(featureCollection);
+
+        PaginatedFieldResponse<FeatureSimpleDTO> fields = fieldService.getAllFeatureCollectionSimpleDTO(name, soil, status, culture,  harvest, farmName, page, itens);
+
+        return ResponseEntity.ok(fields);
     }
 
     @GetMapping("/featureCollection/{id}")
