5e64f3acb4a068f35421aad8fc1a1f92a87e120e
gabriel15asouza <gabalves.souza@gmail.com>
2025-04-15 01:56:19 -0300

    [feat]: criacao de entidade paginacao

---
+++ src/main/java/com/morpheus/backend/DTO/PaginatedFieldResponse.java
@@ -0,0 +1,19 @@
+package com.morpheus.backend.DTO;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaginatedFieldResponse<T> {
+    private List<T> field;
+    private int totalPages;
+    private long totalElements;
+
+}
