fe80cd8883b80c07ec6a0604f115f8205a8c2426
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-04-19 18:26:26 -0300

    Refactor(#36): alterado o método getCultureById para utilizar @PathVariable ao invés de RequestBody e reorganizado anotações

--- src/main/java/com/morpheus/backend/controller/CultureController.java
+++ src/main/java/com/morpheus/backend/controller/CultureController.java
@@ -1,34 +1,37 @@
 package com.morpheus.backend.controller;
 
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.morpheus.backend.DTO.CultureDTO;
 import com.morpheus.backend.entity.Culture;
 import com.morpheus.backend.service.CultureService;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/cultures")
 public class CultureController {
-    
+
     @Autowired
     private CultureService cultureService;
-    
+
     @GetMapping
     public List<Culture> getAllCultures() {
         return cultureService.getAllCultures();
     }
 
     @GetMapping("/{id}")
-    public Culture getCultureById(@RequestBody Long id) {
+    public Culture getCultureById(@PathVariable Long id) {
         return cultureService.getCultureById(id);
     }
 
