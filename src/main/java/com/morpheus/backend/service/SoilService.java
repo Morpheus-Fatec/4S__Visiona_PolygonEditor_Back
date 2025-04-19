ac973f3701d1f029e820509d2eb3937cce846e1a
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-04-19 19:18:28 -0300

    Feat(#36): criado os métodos getSoilById, updateSoil e deleteSoil com validação de associação a talhões

--- src/main/java/com/morpheus/backend/service/SoilService.java
+++ src/main/java/com/morpheus/backend/service/SoilService.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.morpheus.backend.DTO.SoilDTO;
+
 import com.morpheus.backend.entity.Soil;
+import com.morpheus.backend.repository.FieldRepository;
 import com.morpheus.backend.repository.SoilRepository;
 import com.morpheus.exceptions.DefaultException;
 
@@ -13,23 +14,10 @@ import com.morpheus.exceptions.DefaultException;
 public class SoilService {
 
     @Autowired
-    private SoilRepository soilRepository;
+    public SoilRepository soilRepository;
 
-    public String createSoil(SoilDTO soilName) {
-        try {
-            if (soilName.getName().isEmpty()) {
-                throw new Exception();
-            }
-
-            Soil soil = new Soil();
-            soil.setName(soilName.getName());
-            soilRepository.save(soil);
-
-            return "Tipo de solo " + soilName.getName() +  " criado com sucesso.";
-        } catch (Exception e) {
-            throw new DefaultException("Não foi possível criar o tipo de solo.");
-        }
-    }
+    @Autowired
+    public FieldRepository fieldRepository;
 
     public List<Soil> getAllSoils() {
         try {
@@ -40,8 +28,86 @@ public class SoilService {
             }
 
             return soils;
+        } catch (DefaultException e) {
+            throw e;
         } catch (Exception e) {
             throw new DefaultException("Não foi possível encontrar tipos de solo.");
+        }
+    }
+
+    public Soil getSoilById(Long id) {
+        try {
+            Soil soil = soilRepository.findById(id).orElseThrow(Exception::new);
+            return soil;
+        } catch (DefaultException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new DefaultException("Não foi possível encontrar o tipo de solo.");
+        }
+    }
+
+    public String createSoil(String soilName) {
+        try {
+            if (soilName == null || soilName.isEmpty()) {
+                throw new DefaultException("O nome do solo não pode ser vazio ou nulo.");
+            }
+
+            Soil soil = new Soil();
+            soil.setName(soilName);
+            soilRepository.save(soil);
+
+            return "Tipo de solo " + soil.getName() +  " criado com sucesso.";
+        } catch (DefaultException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new DefaultException("Não foi possível criar o tipo de solo.");
+        }
+    }
+
+    public String updateSoil(Long id, String soilName) {
+        try {
+            Soil soil = soilRepository.getSoilById(id);
+
+            if(soil == null) {
+                throw new Exception();
+            }
+
+            if (soilName == null || soilName.isEmpty()) {
+                throw new Exception();
+            }
+
+            String oldSoild = soil.getName();
+            soil.setName(soilName);
+            soilRepository.save(soil);
+
+            return "Tipo de solo atualizado de " + oldSoild + " para " + soilName + " com sucesso.";
+        } catch (DefaultException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new DefaultException("Não foi possível atualizar o tipo de solo.");
+        }
+    }
+
+    public String deleteSoilById(Long id) {
+        try {
+            if (id == null) {
+                throw new Exception();
+            }
+            Soil soil = soilRepository.getSoilById(id);
+            
+            if(soil == null) {
+                throw new Exception();
+            }
+            if(fieldRepository.existsBySoil_Name(soil.getName())) {
+                throw new DefaultException("Não é possível deletar o tipo de solo, pois ele está vinculado a um campo.");
+            }
+            
+            soilRepository.delete(soil);
+            return "Tipo de solo " + soil.getName() + " deletado com sucesso.";
+        } catch (DefaultException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new DefaultException("Não foi possível deletar o tipo de solo.");
         }
     }
 }
