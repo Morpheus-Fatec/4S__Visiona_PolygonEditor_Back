acbfd0bc674ac1c8e5b80c842df56b82bf14b92d
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-04-19 18:36:46 -0300

    Feat(#36): adicionado o método deleteCulture para remover uma cultura pelo ID

--- src/main/java/com/morpheus/backend/controller/CultureController.java
+++ src/main/java/com/morpheus/backend/controller/CultureController.java
@@ -1,10 +1,9 @@
 package com.morpheus.backend.controller;
 
-
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.morpheus.backend.DTO.CultureDTO;
 import com.morpheus.backend.entity.Culture;
 import com.morpheus.backend.service.CultureService;
-
 
 @RestController
 @RequestMapping("/cultures")
@@ -46,4 +43,8 @@ public class CultureController {
         return cultureService.updateCulture(id, culture.getName());
     }
 
+    @DeleteMapping("/{id}")
+    public String deleteCulture(@PathVariable Long id) {
+        return cultureService.deleteCulture(id);
+    }
 }
