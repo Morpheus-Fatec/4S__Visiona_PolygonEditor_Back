d7e745235a9d3c5721bbf2a5520ff83605aa6f71
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2025-05-24 14:35:31 -0300

    Feat(#59): Configuração para requisições autenticadas.

---
+++ src/main/java/com/morpheus/backend/config/SecurityFilter.java
@@ -0,0 +1,62 @@
+package com.morpheus.backend.config;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.morpheus.backend.repository.UserRepository;
+import com.morpheus.backend.service.TokenService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+    
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        
+        if (token != null) {
+            var email = tokenService.validateToken(token);
+            
+            if (email != null) {
+                UserDetails user = userRepository.findByEmail(email);
+
+                if (user != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } else {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Usuário não encontrado.");
+                    return;
+                }
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token inválido.");
+                return;
+            }
+        }
+        
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
+    }
+}
