d9c9af6f8e13e14729a817860baf66c70164f4c1
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2025-05-24 14:36:09 -0300

    Feat(#59): Implementação de criptografia de senhas.

--- src/main/java/com/morpheus/backend/entity/User.java
+++ src/main/java/com/morpheus/backend/entity/User.java
@@ -1,5 +1,12 @@
 package com.morpheus.backend.entity;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Table(name = "Usuarios")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,4 +41,36 @@ public class User {
 
     @Column(name = "analista", nullable = false)
     private Boolean isAnalyst;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.isAdmin) {return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),new SimpleGrantedAuthority("ROLE_ANALYST"),new SimpleGrantedAuthority("ROLE_CONSULTANT"));}
+        else if(this.isAnalyst){return List.of(new SimpleGrantedAuthority("ROLE_ANALYST"));}
+        else {return List.of(new SimpleGrantedAuthority("ROLE_CONSULTANT"));}
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+		return true;
+	}
+
+    @Override
+    public boolean isAccountNonLocked() {
+		return true;
+	}
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+    @Override
+    public boolean isEnabled() {
+		return true;
+	}
 }
--- src/main/java/com/morpheus/backend/repository/UserRepository.java
+++ src/main/java/com/morpheus/backend/repository/UserRepository.java
@@ -1,8 +1,7 @@
 package com.morpheus.backend.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import com.morpheus.backend.entity.User;
@@ -10,7 +9,6 @@ import com.morpheus.backend.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     User getUserById(Long id);
-    Optional<User> findByEmail(String email);
-
-    
+    UserDetails findByEmail(String email);
+    User getUserByEmail(String email);    
 }
